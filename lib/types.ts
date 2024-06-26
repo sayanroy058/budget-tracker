56263e39ae54973caf890a97f06bb8a8732cf4f6
Sayan Roy <sayanroy058@gmail.com>
2024-06-27 03:11:36 +0530

    Initial Commit

---
+++ lib/types.ts
@@ -0,0 +1,4 @@
+// In "@/lib/types" or where the types are defined
+export type TransactionType = "income" | "expense" | "savings";
+export type Timeframe = "month" | "year";
+export type Period = { year: number; month: number };
